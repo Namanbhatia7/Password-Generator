313ee27d6ed483879eba8741c48de7f71fa5d12b
Namanbhatia7 <52316491+Namanbhatia7@users.noreply.github.com>
2020-08-18 01:37:41 +0530

    generic code

--- pass_generate.py
+++ pass_generate.py
@@ -4,18 +4,10 @@ import random
 
 
 def gen_password(length):
-    
-    case1 = string.ascii_lowercase;
-    case2 = string.ascii_uppercase;
-    case3 = string.digits;
-    case4 = string.punctuation;
-    pickable = case1 + case2 + case3 + case4
+    pickable = string.ascii_lowercase + string.ascii_uppercase + string.digits + string.punctuation; 
     password = ''.join(random.choice(pickable) for i in range(length))
     print('Password Suggestion :', password)
-
 
 if __name___=='__main':
     leng = int(input('Enter the Length of Password you want to generate: '))
     gen_password(leng)
-
-
